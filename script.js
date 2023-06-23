6be164d79589317ade3194d524020332de713250
aryaabhi023 <abhishekparmar2306@gmail.com>
2023-06-23 22:15:57 +0530

    <>

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const id6 = document.getElementById("6");
 
 id1.addEventListener("dblclick", () =>
   alert(
-    "You know shubhangi my heart always beat faster when i am thinKing about u"
+    "You know mam my heart always beat faster when i am thinKing about u. Is it alright or mujhe koi bemari hain?"
   )
 );
 id2.addEventListener("dblclick", () =>
